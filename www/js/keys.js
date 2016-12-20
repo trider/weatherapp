44c78294d4b6f7a1704397903e970e664af91cc1
Jonathan Gold <jonnygold@gmail.com>
2016-12-20 20:44:28 +0200

    Ver 0.1.1: Minor change to keys.js

--- www/js/keys.js
+++ www/js/keys.js
@@ -1,5 +1,5 @@
 /**
  * Created by jonnygold on 20/12/2016.
  */
-var key='AIzaSyDtiY4QKJgJd3fTAgI3Oi7orl2xROY9eGE';
-var appid='990584634987be651d36ebf9c57752f3';
+var key='your google maps key';
+var appid='your openweathermap appid';
